69fcbc941864e8c2f0b7a0eab43e951922dc7b18
하이미디어학원 <@DESKTOP-1NQ75LQ>
2024-03-28 16:59:25 +0900

    240328

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,7 +3,7 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
-        //코드작성
+        //코드 작성
     	if("음악".equals(str)){
             playMusic();
         } else if("앱".equals(str)) {
